3fac5268783f9d175a1c02bffd5fb25c5b857035
ryanboris <ryan.boris@gmail.com>
2019-07-26 13:00:42 -0400

    :recycle: Complete stretch refactor

--- names/names.py
+++ names/names.py
@@ -12,15 +12,72 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 
+# duplicates = []
+# names = names_1 + names_2
+# cache = {}
+# for name in names:
+#     if name not in cache:
+#         cache[name] = 1
+#     else:
+#         cache[name] += 1
+#         duplicates.append(name)
+
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = BinarySearchTree(value)
+                else:
+                    self.left.insert(value)
+            elif value > self.value:
+                if self.right is None:
+                    self.right = BinarySearchTree(value)
+                else:
+                    self.right.insert(value)
+        else:
+            self.value = value
+
+    def contains(self, target):
+        if target < self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        elif target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+        else:
+            return True
+
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+
 duplicates = []
-names = names_1 + names_2
-cache = {}
-for name in names:
-    if name not in cache:
-        cache[name] = 1
-    else:
-        cache[name] += 1
+names1_bt = BinarySearchTree(names_1[0])
+for name in names_1[1:]:
+    names1_bt.insert(name)
+for name in names_2:
+    if names1_bt.contains(name):
         duplicates.append(name)
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
