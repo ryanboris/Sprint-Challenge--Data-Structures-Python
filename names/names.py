291a3a7d226c883e2d308cf7cd3f91d1e45eaba9
ryanboris <ryan.boris@gmail.com>
2019-07-26 11:58:30 -0400

    :construction: Optimize duplicates algorithm

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +11,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names = names_1 + names_2
+cache = {}
+for name in names:
+    if name not in cache:
+        cache[name] = 1
+    else:
+        cache[name] += 1
+        duplicates.append(name)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
